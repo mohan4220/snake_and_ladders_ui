58cd7ace908cf066d10ea78672c2bcc06a4d1418
mohan <mohan.krishna4220@gmail.com>
2023-12-21 00:32:10 +0530

    game pos calculation

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -3,11 +3,44 @@ import { boardHeight, boardWidth } from "./utilities/constants";
 import "./styles/globalStyles.css";
 import { SpherePawn } from "./components/SpherePawn";
 import { pawnAttributes } from "./utilities/constants";
+import { useEffect, useState } from "react";
 
 const bWidth = boardWidth / 10;
 const bHeight = boardHeight / 10;
 
+const ladders = {
+  2: 23,
+  4: 68,
+  6: 45,
+  20: 59,
+  30: 95,
+  50: 5,
+  43: 17,
+  52: 72,
+  56: 8,
+  57: 96,
+  73: 15,
+  71: 92,
+  84: 58,
+  87: 49,
+  98: 40,
+};
+
+const locationCalc = (pos) => {
+  let hOffset = Math.floor(pos / 10);
+  let wOffset = 0;
+  //pos = pos - 1;
+  if (Math.floor(pos / 10) % 2 === 0) {
+    wOffset = pos % 10;
+  } else {
+    wOffset = 10 - (pos % 10) - 1;
+  }
+  return [hOffset, wOffset];
+};
+
 export const GameBoard = () => {
+  const [pos, setPos] = useState(0);
+
   return (
     <>
       <div className="board-container">
@@ -23,13 +56,20 @@ export const GameBoard = () => {
               primary={pawn.primary}
               secondary={pawn.secondary}
               positionStyling={true}
-              left={pawn.leftOffset + 9 * bWidth}
-              bottom={pawn.bottomOffset}
+              left={pawn.leftOffset + locationCalc(pos)[1] * bWidth}
+              bottom={pawn.bottomOffset + locationCalc(pos)[0] * bHeight}
             />
           ))}
         </div>
         <div className="game-metrics">
-          you are wonderful
+          you are wonderful {pos + 1}
+          <button
+            onClick={() => {
+              setPos(Math.min(pos + 1, 99));
+            }}
+          >
+            click
+          </button>
           <SpherePawn primary={"darkblue"} secondary="aqua" />
           <SpherePawn primary="#b71c1c" secondary="#ef9a9a" />
           <SpherePawn primary="#004d40" secondary="#80cbc4" />
